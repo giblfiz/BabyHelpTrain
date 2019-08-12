e5e13bc74fef62fe43837ed768d0931de48a648b
Harry Pottash <giblfiz@gmail.com>
2019-08-12 11:30:27 +0530

    fixed demo to use secrets file, so now it works on github

--- TwillioDemo.php
+++ TwillioDemo.php
@@ -1,16 +1,17 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 use Twilio\Rest\Client;
+require __DIR__ . '/config/secrets.php';
 
 // Your Account SID and Auth Token from twilio.com/console
-$account_sid = '';
-$auth_token = '';
+$account_sid = TWILLIO_ACCOUNT_SID;
+$auth_token = TWILLIO_AUTH_TOKEN;
 // In production, these should be environment variables. E.g.:
 // $auth_token = $_ENV["TWILIO_ACCOUNT_SID"]
 
 // A Twilio number you own with SMS capabilities
-$twilio_number = "+18177569652";
-
+$twilio_number = "+16108548401";
+//(610) 854-8401
 
 $client = new Client($account_sid, $auth_token);
 $client->messages->create(
